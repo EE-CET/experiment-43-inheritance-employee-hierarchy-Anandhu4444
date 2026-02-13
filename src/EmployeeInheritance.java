9a92e69ca859f1a1c35fe9fe61a32a2370e2ca17
Anandhu4444 <hey4444567889987@gmail.com>
2026-02-13 10:29:51 +0530

    Update EmployeeInheritance.java

--- src/EmployeeInheritance.java
+++ src/EmployeeInheritance.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 
-
 class Employee {
     String name;
     int age;
@@ -23,37 +22,33 @@ class Manager extends Employee {
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
-        
+        // Officer Details
         Officer officer = new Officer();
         officer.name = sc.nextLine();
-        officer.age = sc.nextInt();
-        sc.nextLine();
+        officer.age = Integer.parseInt(sc.nextLine());
         officer.phoneNumber = sc.nextLine();
         officer.address = sc.nextLine();
-        officer.salary = sc.nextDouble();
-        sc.nextLine();
+        officer.salary = Double.parseDouble(sc.nextLine());
         officer.specialization = sc.nextLine();
 
+        // Manager Details
         Manager manager = new Manager();
         manager.name = sc.nextLine();
-        manager.age = sc.nextInt();
-        sc.nextLine();
+        manager.age = Integer.parseInt(sc.nextLine());
         manager.phoneNumber = sc.nextLine();
         manager.address = sc.nextLine();
-        manager.salary = sc.nextDouble();
-        sc.nextLine();
+        manager.salary = Double.parseDouble(sc.nextLine());
         manager.department = sc.nextLine();
 
-
+        // Output
         System.out.println("Officer:");
         System.out.println(officer.name);
         System.out.println(officer.age);
         System.out.println(officer.phoneNumber);
         System.out.println(officer.address);
-        System.out.println(officer.salary);
+        officer.printSalary();
         System.out.println(officer.specialization);
 
         System.out.println("Manager:");
@@ -61,10 +56,9 @@ public class Main {
         System.out.println(manager.age);
         System.out.println(manager.phoneNumber);
         System.out.println(manager.address);
-        System.out.println(manager.salary);
+        manager.printSalary();
         System.out.println(manager.department);
 
-        
+        sc.close();
     }
 }
-
